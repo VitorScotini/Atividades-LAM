3416443ade20f6c65ddca0960b9261b4b05eb3d8
VitorScotini <vitorscotini11@gmail.com>
2018-03-26 10:20:10 -0300

    jogo da adivinhaçao versao 2 quase pronto

---
+++ Jogo3TentativasAtualizado/app/src/main/java/com/example/alunos/myapplication/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.alunos.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.EditText;
+import java.util.*;
+
+public class MainActivity extends AppCompatActivity {
+    Random numero = new Random();
+    int x = (numero.nextInt(1000))+1;
+    int tent = 0;
+    int nmr;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void Jogar(View view) {
+        Scanner input = new Scanner(System.in);
+        TextView resultado;
+        TextView tentativas;
+        resultado = findViewById(R.id.resultado);
+        tentativas = findViewById(R.id.tentativas);
+
+        EditText userInput = findViewById(R.id.editText);
+        String teste = userInput.getText().toString();
+        nmr = Integer.parseInt(teste);
+
+        if (nmr > x) {
+            resultado.setText("O número é menor");
+        }else if (nmr < x) {
+            resultado.setText("O número é maior!");
+        }else{
+            resultado.setText("Ganhou!!!");
+            tentativas.setText(Integer.toString(tent));
+        }
+        tent = tent + 1;
+
+    }
+}
