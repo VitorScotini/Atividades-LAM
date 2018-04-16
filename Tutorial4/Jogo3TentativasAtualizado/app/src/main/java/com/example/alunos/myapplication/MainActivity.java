a5b7d226d8e3983f2cbf0c403301a9e44315a771
VitorScotini <vitorscotini11@outlook.com>
2018-04-16 16:41:08 -0300

    so pra salvar um trecho de 2 linhas de codigo

--- Tutorial4/Jogo3TentativasAtualizado/app/src/main/java/com/example/alunos/myapplication/MainActivity.java
+++ Tutorial4/Jogo3TentativasAtualizado/app/src/main/java/com/example/alunos/myapplication/MainActivity.java
@@ -13,10 +13,10 @@ import com.example.alunos.myapplication.Ver_placar;
 public class MainActivity extends AppCompatActivity {
     Random numero = new Random();
     int x = (numero.nextInt(1000))+1;
-    int tent = 0;
+    int i,tent = 0;
     int nmr;
     TextView tentativas,resultado;
-
+    int vetor[] = new int[5];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,33 +26,54 @@ public class MainActivity extends AppCompatActivity {
 
     public void Jogar(View view) {
         Scanner input = new Scanner(System.in);
-        resultado = findViewById(R.id.resultado);
         tentativas = findViewById(R.id.tentativas);
+        resultado = findViewById(R.id.resultado);
 
         EditText userInput = findViewById(R.id.editText);
         String teste = userInput.getText().toString();
         nmr = Integer.parseInt(teste);
 
-        if (nmr > x) {
-            resultado.setText("O número é menor");
+        if (nmr > 1000 ) {
+            resultado.setText("O número é menor que 1000");
         }else if (nmr < x) {
             resultado.setText("O número é maior!");
+        }else if(nmr > x){
+            resultado.setText("O número é menor");
         }else{
-            resultado.setText("Ganhou!!!");
+            resultado.setText("Gaanhouuuuuuuuuuuuuuuuuuuuuu !!!!!!!!! ");
+            for(i=0;i < 4;i++){
+                vetor[i] = vetor[i+1];
+            }
+            vetor[4] = tent + 1;
         }
         tent = tent + 1;
         tentativas.setText(Integer.toString(tent));
+
     }
 
     public void Ver(View v){
         Intent intent = new Intent(this,Ver_placar.class);
+        Bundle b = new Bundle();
+        b.putInt("vet",vetor[4]);
+        b.putInt("vet2",vetor[3]);
+        b.putInt("vet3",vetor[2]);
+        b.putInt("vet4",vetor[1]);
+        b.putInt("vet5",vetor[0]);
+        intent.putExtras(b);
         startActivity(intent);
     }
 
-    public void deNovo(View v){
-        x = (numero.nextInt(1000))+1;
-        tent = 0;
-        tentativas.setText(Integer.toString(tent));
-        resultado.setText("Joga denovo aê");
+    public void Ganhou(View v){
+        if(resultado.getText() == "Gaanhouuuuuuuuuuuuuuuuuuuuuu !!!!!!!!! ") {
+            EditText userInput2 = findViewById(R.id.editText);
+            x = (numero.nextInt(1000)) + 1;
+            tent = 0;
+            tentativas.setText(Integer.toString(tent));
+            resultado.setText("");
+            userInput2.setText("");
+        }else{
+            resultado.setText("Você não ganhou para pode jogar de novo");
+        }
+
     }
 }
--- Tutorial4/Jogo3TentativasAtualizado/app/src/main/java/com/example/alunos/myapplication/Ver_placar.java
+++ Tutorial4/Jogo3TentativasAtualizado/app/src/main/java/com/example/alunos/myapplication/Ver_placar.java
@@ -1,6 +1,7 @@
 package com.example.alunos.myapplication;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,7 +9,6 @@ import android.widget.TextView;
 import android.widget.EditText;
 import java.util.*;
 
-import com.example.alunos.myapplication.MainActivity;
 
 public class Ver_placar extends AppCompatActivity {
 
@@ -16,6 +16,51 @@ public class Ver_placar extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ver_placar);
+        TextView t1, t2, t3, t4, t5, tchefe;
+
+        t1 = findViewById(R.id.t1);
+        t2 = findViewById(R.id.t2);
+        t3 = findViewById(R.id.t3);
+        t4 = findViewById(R.id.t4);
+        t5 = findViewById(R.id.t5);
+        tchefe = findViewById(R.id.tchefe);
+
+        Intent intent = getIntent();
+        Bundle b = intent.getExtras();
+
+        Integer menor;
+
+        Integer t = b.getInt("vet5");
+        Integer te = b.getInt("vet4");
+        Integer ten = b.getInt("vet3");
+        Integer tent = b.getInt("vet2");
+        Integer tentat = b.getInt("vet");
+
+        t1.setText(t.toString());
+        t2.setText(te.toString());
+        t3.setText(ten.toString());
+        t4.setText(tent.toString());
+        t5.setText(tentat.toString());
+
+        int[] a = new int[5];
+        a[0] = t;
+        a[1] = te;
+        a[2] = ten;
+        a[3] = tent;
+        a[4] = tentat;
+        menor = t;
+        for (int i = 0; i < 5; i++) {
+            if (a[i] < menor) {
+                menor = a[i];
+            }
+        }
+        tchefe.setText(menor.toString()); ;
+
+    }
+
+    public void Voltar(View view) {
+        Intent in = new Intent(this, MainActivity.class);
+        startActivity(in);
     }
 
 }
