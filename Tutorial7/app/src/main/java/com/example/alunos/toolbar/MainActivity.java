ba871eb72e7659b91c8d748d58ce4377dd07fb2c
VitorScotini <vitorscotini11@outlook.com>
2018-06-19 15:11:47 -0300

    pai

--- Tutorial7/app/src/main/java/com/example/alunos/toolbar/MainActivity.java
+++ Tutorial7/app/src/main/java/com/example/alunos/toolbar/MainActivity.java
@@ -11,7 +11,7 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
-import android.view.MenuItem; 
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
